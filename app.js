1b473accb02d8b0d4f1dd7110130189a355818ca
utkarsh <utkarshgubrelay2000@gmail.com>
2020-11-19 01:20:37 +0530

    initial comit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+const app=express()
+const cors = require("cors")
+const mongoose=require('mongoose')
+require("dotenv").config({path:"./config/config.env"})
+
+var Router = require('./routes/User');
+mongoose.connect(process.env.MONGO_URL,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+});
+
+mongoose.connection.on("connected", () => {
+console.log("connected to mongo yeah !");
+});
+mongoose.connection.on("error",()=>{
+  console.log("error connecting to mongo " ,)
+}).catch(err =>{
+console.log("error is",err)
+})
+
+
+app.use(cors())
+app.use(express.json())
+app.use('/',Router)
+
+
+app.listen(process.env.PORT, () => {
+  console.log("running on PORT ",process.env.PORT);
+});
+
+module.exports = app;
